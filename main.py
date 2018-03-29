cc6ebdde1c2beae4f58af970a3a3dbcbbe0f4c48
wenjietseng <wenjietseng@gmail.com>
2018-03-29 12:37:29 +0800

    update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
-from models import resnet
+from models.resnet import ResNet18
 from utils import progress_bar
 from torch.autograd import Variable
 
